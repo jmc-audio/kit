24a396f9599e01e6fcb5aa3184af8315039d85c6
jmcgill <wyvernwy@cox.net>
2016-03-29 14:59:31 -0700

    pass principal from authn to authz

--- auth/middleware.go
+++ auth/middleware.go
@@ -2,6 +2,7 @@ package auth
 
 import (
 	"github.com/go-kit/kit/endpoint"
+	"github.com/pborman/uuid"
 	"golang.org/x/net/context"
 )
 
@@ -11,26 +12,26 @@ type Authenticator interface {
 }
 
 type authenticator struct {
+	uuid  string
 	authN AuthNFunc
 	authZ AuthZFunc
 }
 
 func NewAuthenticator(authN AuthNFunc, authZ AuthZFunc) Authenticator {
-	return &authenticator{authN: authN, authZ: authZ}
+	return &authenticator{uuid.New(), authN: authN, authZ: authZ}
 }
 
 func (a *authenticator) Authenticated() endpoint.Middleware {
 	return func(next endpoint.Endpoint) endpoint.Endpoint {
 		return func(ctx context.Context, i interface{}) (interface{}, error) {
-			var ok bool
-			var p Principal
+			var (
+				ok bool
+				p  Principal
+			)
 
 			if p, ok = i.(Principal); ok {
-				if p == nil || p.PrincipalToken() == nil {
-					return nil, &UnknownPrincipal{}
-				}
 				if a.authN(p) {
-					return next(ctx, i)
+					return next(context.WithValue(ctx, a.uuid, p), i)
 				}
 				return nil, &Unauthenticated{}
 			}
@@ -44,14 +45,19 @@ func (a *authenticator) Authenticated() endpoint.Middleware {
 func (a *authenticator) Authorized() endpoint.Middleware {
 	return func(next endpoint.Endpoint) endpoint.Endpoint {
 		return func(ctx context.Context, i interface{}) (interface{}, error) {
-			var ok bool
-			var s Subject
-
+			var (
+				ok bool
+				s  Subject
+				p  Principal
+			)
+			if p, ok = ctx.Value(uuid).(Principal); !ok {
+				return nil, &UnknownPrincipal{}
+			}
 			if s, ok = i.(Subject); ok {
 				if s == nil {
 					return nil, &UnknownSubject{}
 				}
-				if a.authZ(s) {
+				if a.authZ(p, s) {
 					return next(ctx, i)
 				}
 				return nil, &Unauthorized{}
